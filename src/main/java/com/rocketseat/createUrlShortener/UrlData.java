2e4bcc1e015811a61ff32369a5fea7053cd64c5b
Raul Silveira <raulantsilveira@gmail.com>
2024-11-20 20:23:39 -0300

    connect s3 bucket to lambda to save data

---
+++ src/main/java/com/rocketseat/createUrlShortener/UrlData.java
@@ -0,0 +1,17 @@
+package com.rocketseat.createUrlShortener;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class UrlData {
+
+    private String originalUrl;
+    private long expirationTime;
+
+    public UrlData(String originalUrl, long expirationTimeInSeconds) {
+    }
+}
